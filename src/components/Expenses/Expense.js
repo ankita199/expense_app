37793f34765ad7daae9cec2fa8bd3237e33bf522
ankita <ankita.agrawal1113@gmail.com>
2023-08-31 18:48:01 -0700

    Expense Chart added

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -2,6 +2,8 @@ import ExpenseFilter from './ExpenseFilter';
 import './Expenses.css';
 import React, { useState } from 'react'
 import ExpenseList from './ExpenseList.js'
+import ExpensesChart from './ExpensesChart.js'
+import Card from '../UI/Card';
 
 function Expense(props) {
   const [filteredYear, setFilteredYear] = useState('2021')
@@ -9,15 +11,17 @@ function Expense(props) {
     setFilteredYear(selectedYear)
   }
   const filteredExpenses = props.items.filter((expense) => (
-    expense.date.getFullYear() === filteredYear
+    expense.date.getFullYear().toString() === filteredYear
   ))
+
   return (
     <div>
-      <div className="expenses">
+      <Card className='expenses'>
         <ExpenseFilter selectedYear={filterExpenses} defaultYear={filteredYear} />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpenseList items={filteredExpenses} />
-      </div>
-    </div>
+      </Card>
+    </div >
   );
 }
 
--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -2,11 +2,12 @@ import './ExpenseList.css';
 import ExpenseItem from './ExpenseItem';
 
 function ExpenseList(props) {
+  console.log(props.items)
   if(props.items.length === 0){
     return(<p>No Expenses found!!</p>)
   }
   return (
-    <ul className="expense-list">
+    <ul className='expenses-list'>
       {
         props.items.map((expense) => (
           <ExpenseItem key={expense.id} date={expense.date} title={expense.title} amount={expense.amount} />
